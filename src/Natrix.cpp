0a84ff0fa5228b7243ae288dcfc764682ac005ac
Daniil Panov <daniil_panov2005@mail.ru>
2023-06-27 16:57:45 +0200

    feat: add second universal function (without limits); maybe established

--- src/Natrix.cpp
+++ src/Natrix.cpp
@@ -6,10 +6,16 @@ Move search_move_special(const std::string& position, uint8_t en_passant, bool w
     AI ai;
     return ai.best_move_special(pos, pieces, depth);
 }
+Move search_move(const std::string& position, uint8_t en_passant, bool w_l_castling, bool w_s_castling, bool b_l_castling, bool b_s_castling, uint8_t pieces, float move_ctr) {
+    Position pos = {position, en_passant, w_l_castling, w_s_castling, b_l_castling, b_s_castling, move_ctr};
+    AI ai;
+    return ai.best_move(pos, pieces, 0, 600);
+}
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(Natrix)
 {
-    function("search_move_special", &search_move_special);
+    function("search_move_special", &search_move_special),
+    function("search_move", &search_move);
 }
